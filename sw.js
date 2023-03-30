defcedc0443e2e8a0db0b5501dbfff480e67139b
yeftawatson <yeftanathaniel16@gmail.com>
2023-03-30 21:33:34 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,14 +4,7 @@ const urlsToCache = [
   '/index.html',
   '/styles.css',
   '/script.js',
-  '/icons/icon-72x72.png',
-  '/icons/icon-96x96.png',
-  '/icons/icon-128x128.png',
-  '/icons/icon-144x144.png',
-  '/icons/icon-152x152.png',
-  '/icons/icon-192x192.png',
-  '/icons/icon-384x384.png',
-  '/icons/icon-512x512.png'
+
 ];
 
 self.addEventListener('install', function(event) {
